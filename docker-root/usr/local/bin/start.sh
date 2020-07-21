ab38487e614d5f348b82111092ad3c800e01d19b
Hagb Green <hagb_green@qq.com>
2020-07-21 20:58:16 +0800

    Use iptables-legacy if nftables not supported
    Thank @BoringCat https://github.com/Hagb/docker-easyconnect/issues/5

--- docker-root/usr/local/bin/start.sh
+++ docker-root/usr/local/bin/start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# 不支持 nftables 时使用 iptables-legacy
+# 感谢 @BoringCat https://github.com/Hagb/docker-easyconnect/issues/5
+if { iptables-nft -L 1>/dev/null 2>/dev/null ;}
+then
+	update-alternatives --set iptables /usr/sbin/iptables-nft
+	update-alternatives --set ip6tables /usr/sbin/ip6tables-nft
+else
+	update-alternatives --set iptables /usr/sbin/iptables-legacy
+	update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+fi
+
 # 在虚拟网络设备 tun0 打开时运行 proxy 代理服务器
 [ -n "$NODANTED" ] || (while true
 do
