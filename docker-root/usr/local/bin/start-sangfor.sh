2d5c909e80592c7b81c901cf112342cd6f213b8a
Hagb <hagb@hagb.name>
2022-09-24 09:54:58 +0800

    Avoid login failure in poor-performance environment
    When logining, ECAgent starts to communicate with CSClient via a fifo
    file. In 7.6.3, ECAgent can't starts CSClient and svpnservice by itself,
    so our script start these two programs once ECAgent begins to complain
    the communication failure. In poor-performance environment (such as slow
    device and docker with some limits set by user), ECAgent may reach its
    timeout before CSClient is ready, and then login will fail.
    
    To avoid such an error, this commit suspends the ECAgent process before
    starts CSClient and svpnservice, and continues ECAgent after the fifo
    file is created by CSClient.

--- docker-root/usr/local/bin/start-sangfor.sh
+++ docker-root/usr/local/bin/start-sangfor.sh
@@ -3,7 +3,6 @@ fake-hwaddr-run() { "$@" ; }
 if [ -n "$FAKE_HWADDR" ]; then
 	fake-hwaddr-run() { LD_PRELOAD=/usr/local/lib/fake-hwaddr.so "$@" ; }
 fi
-[ -z "$_EC_CLI" ] && /usr/share/sangfor/EasyConnect/resources/bin/EasyMonitor
 
 # 对 qemu-user 内存泄漏 https://gitlab.com/qemu-project/qemu/-/issues/866 的一个 workaround
 # 在 ECAgent 超过一定内存时杀掉它，经试验并不会对 vpn 服务造成很大影响
@@ -26,15 +25,31 @@ fi
 } &
 
 sleep 1
+
 while true
 do
+	rm /usr/share/sangfor/EasyConnect/resources/conf/ECDomainFile
 	if [ -z "$_EC_CLI" ]; then
-		# 在 EasyConnect 前端启动过程中，会出现 cms client connect failed 的报错，此时应该启动 sslservice.sh。但这个脚本启动得太早也会没有作用……
-		# 参考了 https://blog.51cto.com/13226459/2476193 ，在此对作者表示感谢。
-		{
-			tail -n 0 -f /usr/share/sangfor/EasyConnect/resources/logs/ECAgent.log | grep "\\[Register\\]cms client connect failed" -m 1
-			/usr/share/sangfor/EasyConnect/resources/shell/sslservice.sh
-		} &
+		# 在 EasyConnect 前端启动过程中，会出现 cms client connect failed 的报错，此时应该启动 sslservice.sh。但这个脚本启动得太早也会没有作用。
+		# (来自 https://blog.51cto.com/13226459/2476193 的线索，感谢文章作者)
+		# 进一步研究发现此时应启动 svpnservice 和 CSClient 两个程序
+		fake-hwaddr-run /usr/share/sangfor/EasyConnect/resources/bin/ECAgent |
+			{
+				grep '\[Register\]cms client connect failed|ECDomainFile domain socket connect failed' -Em 1 --line-buffered || break
+				killall -9 svpnservice CSClient
+				# 在某些性能不佳的设备上（尤其是如果使用了 qemu-user 来模拟运行其他架构的 EasyConnect），CSClient 和 svpnservice 启动较慢，
+				# 此时有可能 CSClient 启动完成前 ECAgent 就会等待超时、登录失败，因此启动 CSClient 前先将 ECAgent 休眠（发送 STOP 信号），
+				# CSClient 启动完成（以 fifo 文件 ECDomainFile 存在为标志）后再解除 ECAgent 休眠（发送 CONT 信号）
+				killall -STOP ECAgent
+				fake-hwaddr-run /usr/share/sangfor/EasyConnect/resources/bin/svpnservice -h /usr/share/sangfor/EasyConnect/resources &
+				fake-hwaddr-run /usr/share/sangfor/EasyConnect/resources/bin/CSClient &
+				wait
+				until [ -e /usr/share/sangfor/EasyConnect/resources/conf/ECDomainFile ]; do
+					sleep 0.1
+					pidof ECAgent >/dev/null || exit
+				done
+				killall -CONT ECAgent
+			} &
 
 		# 下面这行代码启动 EasyConnect 的前端。
 		/usr/share/sangfor/EasyConnect/EasyConnect --enable-transparent-visuals --disable-gpu
