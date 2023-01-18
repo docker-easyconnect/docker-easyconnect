f129e7ad6812ea8823b2923146b757c94791cb96
Hagb <hagb@hagb.name>
2023-01-19 00:30:17 +0800

    Move port forwarding to start.sh

--- docker-root/usr/local/bin/detect-route.sh
+++ docker-root/usr/local/bin/detect-route.sh
@@ -27,30 +27,3 @@ else
 
 fi
 ip rule del iif lo sport 1080 table 2
-
-if [ -n "$FORWARD" ]; then
-	if iptables -t mangle -A PREROUTING -m addrtype --dst-type LOCAL -j MARK --set-mark 2; then
-		iptables -t mangle -D PREROUTING -m addrtype --dst-type LOCAL -j MARK --set-mark 2
-		iptables -t nat -A POSTROUTING -p tcp -m mark --mark 2 -j MASQUERADE
-		ip rule add fwmark 2 table 2
-		format_error() { echo Format error in \""$rule"\": "$@" >&2 ; }
-		for rule in $FORWARD; do
-			array=(${rule//:/ })
-			case ${#array[@]} in
-				3) src_args="" ;;
-				4) src_args="-s ${array[0]}" ;;
-				*) format_error; continue ;;
-			esac
-			dst=${array[-2]}:${array[-1]}
-			dport=${array[-3]}
-			match_args="$src_args --dport $dport -m addrtype --dst-type LOCAL -i tun0"
-			iptables -t mangle -A PREROUTING -p tcp $match_args -j MARK --set-mark 2
-			iptables -t mangle -A PREROUTING -p udp $match_args -j MARK --set-mark 2
-			iptables -t nat -A PREROUTING -p tcp $match_args -j DNAT --to-destination $dst
-			iptables -t nat -A PREROUTING -p udp $match_args -j DNAT --to-destination $dst
-
-		done
-	else
-		echo "Can't append iptables used to forward ports from EasyConnect to host network!" >&2
-	fi
-fi
